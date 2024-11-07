8d52de0cf6d05cdb808a41511a4dbe3c494d47f2
Franco <franco_tigres18@hotmail.com>
2024-11-07 08:42:04 -0600

    fix(Appointment):migration, model, routes and controller created

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\VerifyController;
@@ -8,7 +9,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-
 })->middleware('auth:sanctum');
 
 // Verificar Usuario con email
@@ -20,3 +20,5 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::resource('services', ServiceController::class);
+
+Route::apiResource('appointments', AppointmentController::class);
