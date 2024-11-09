d7055ac1c95170655f8f17b68aea8c34ae4228a8
Franco <franco_tigres18@hotmail.com>
2024-11-09 08:05:55 -0600

    feat(Vue):pinia add

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,12 @@
 import "./bootstrap";
 
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./vue/App.vue";
 import router from "./vue/router";
 
 const app = createApp(App);
 
-
+app.use(createPinia());
 app.use(router);
 app.mount("#app");
