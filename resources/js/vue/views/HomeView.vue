67a91042a3532aad05b8291b89ee0791b21521dc
Franco <franco_tigres18@hotmail.com>
2024-11-09 22:56:19 -0600

    feat(axios):config axios

--- resources/js/vue/views/HomeView.vue
+++ resources/js/vue/views/HomeView.vue
@@ -1,14 +1,5 @@
 <script setup>
 import { RouterLink } from "vue-router";
-
-const fetchData = async () => {
-    const res = await fetch(route("services.index"));
-    const data = await res.json();
-
-    console.log(data);
-};
-
-await fetchData();
 </script>
 
 <template>
