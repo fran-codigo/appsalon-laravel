e1b344d234e80322c0146934cd957b14aa28a7f4
Franco <franco_tigres18@hotmail.com>
2024-11-18 15:00:30 -0600

    feat|api-routes|migration:udpate method verify account, add columns to table user

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ServiceController;
-use App\Http\Controllers\VerifyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,13 +10,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Verificar Usuario con email
-Route::get('email/verify/{id}/{hash}', [VerifyController::class, 'verify'])->middleware(['auth:sanctum', 'signed'])->name('verification.verify');
-Route::get('email/resend', [VerifyController::class, 'resend'])->middleware('auth:sanctum')->name('verification.resend');
-
 Route::post('/auth/register', [AuthController::class, 'register'])->name('register.user');
+Route::get('/auth/verify-account/{token}', [AuthController::class, 'verifyUser'])->name('verify.user');
 Route::post('/auth/login', [AuthController::class, 'login'])->name('login.user');
 Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout.user');
+
 
 Route::resource('services', ServiceController::class);
 
