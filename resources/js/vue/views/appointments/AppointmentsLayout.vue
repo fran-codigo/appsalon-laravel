3a55cf37a9557aab8dd95b798e3992cda3427c1a
Franco <franco_tigres18@hotmail.com>
2024-11-30 12:47:09 -0600

    feat|AppointmentsLayout:design add

--- resources/js/vue/views/appointments/AppointmentsLayout.vue
+++ resources/js/vue/views/appointments/AppointmentsLayout.vue
@@ -1,5 +1,38 @@
 <script setup></script>
 
 <template>
-    <RouterView />
+    <div class="container p-10">
+        <h1 class="text-2xl lg:text-5xl font-bold text-center">App Salón</h1>
+
+        <div
+            class="flex flex-col md:flex-row md:justify-center items-center space-y-5 md:space-y-0 mt-5 md:gap-12"
+        >
+            <div class="flex gap-5 items-center">
+                <p class="text-right">Hola: -- Franco -- </p>
+                <button
+                    type="button"
+                    class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg"
+                >
+                    Cerrar sesión
+                </button>
+            </div>
+
+            <nav class="flex gap-5 items-center">
+                <a
+                    href=""
+                    class="p-3 uppercase text-xs font-black rounded-lg hover:underline"
+                    >Mis Citas</a
+                >
+                <a
+                    href=""
+                    class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg bg-blue-700"
+                    >Nueva Cita</a
+                >
+            </nav>
+        </div>
+    </div>
+
+    <main>
+        <RouterView />
+    </main>
 </template>
