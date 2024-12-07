42d073574ef7cd412c27fd1a2224ddd4d5e3b9f9
Franco <franco_tigres18@hotmail.com>
2024-12-06 20:41:02 -0600

    feat|AppointmentView:show calendar and hours

--- resources/js/vue/views/appointments/AppointmentView.vue
+++ resources/js/vue/views/appointments/AppointmentView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import VueTailwindDatePicker from "vue-tailwind-datepicker";
 import { useAppointmentsStore } from "../../store/appointment";
 import SelectedService from "../../components/appointments/SelectedService.vue";
 import { formatCurrency } from "../../utils";
@@ -33,5 +34,31 @@ const appointment = useAppointmentsStore();
                 formatCurrency(appointment.totalAmount)
             }}</span>
         </p>
+    </div>
+
+    <div class="space-y-8" v-if="!appointment.noServiceSelected">
+        <h3 class="text-2xl font-extrabold">Fecha y Hora</h3>
+
+        <div class="lg:flex gap-5 items-start">
+            <div class="w-full lg:w-96 flex justify-center rounded-lg">
+                <VueTailwindDatePicker
+                    i18n="es-mx"
+                    as-single
+                    no-input
+                    v-model="appointment.date"
+                />
+            </div>
+
+            <div
+                class="flex-1 grid grid-cols-1 lg:grid-cols-2 gap-5 mt-5 lg:mt:0"
+            >
+                <button
+                    v-for="hour in appointment.hours"
+                    class="block text-blue-500 rounded-lg text-xl font-black p-3 border border-blue-200"
+                >
+                    {{ hour }}
+                </button>
+            </div>
+        </div>
     </div>
 </template>
